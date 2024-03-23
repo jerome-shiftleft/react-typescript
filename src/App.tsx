2e0270159bd0dce9172465f5cf07d2bde886f12e
jerome-shiftleft <jerome@theshiftleft.com>
2024-03-24 01:31:22 +0800

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-//import Generics from "./lectures/Generics";
 import Todos from "./components/Todos";
 import Todo from "./models/todo";
 import NewTodo from "./components/NewTodo";
 import "./sass/App.scss";
 
-//Generics();
-
 const todos = [new Todo("Learn React"), new Todo("Learn Typescript")];
-console.log('todos: ', todos);
+console.log("todos: ", todos);
 
 function App() {
-
   const addTodoHandler = (text: string) => {
     console.log(`final text: ${text}`);
-  }
+  };
 
   return (
     <>
       <NewTodo onAddTodo={addTodoHandler} />
-      <Todos items={todos} />      
+      <Todos items={todos} />
     </>
   );
 }
