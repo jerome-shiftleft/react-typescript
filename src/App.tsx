81e962e007ef02d84b475b7ace5f4a331a0791c7
jerome-shiftleft <jerome@theshiftleft.com>
2024-03-24 03:25:56 +0800

    Refactor addTodoHandler and removeTodoHandler functions

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,23 @@ import "./sass/App.scss";
 export default function App() {
   const [todos, setTodos] = useState<Todo[]>([]);
 
-  function  addTodoHandler(todoText: string) {
+  function addTodoHandler(todoText: string) {
     console.log(`final text: ${todoText}`);
     const newTodo = new Todo(todoText);
     setTodos((prevTodos) => {
       //return prevTodos.concat(newTodo);
       return [...prevTodos, newTodo];
     });
-  };
+  }
 
   function removeTodoHandler(id: string) {
     console.log(`removing ${id}`);
+    setTodos((prevTodos) => {
+      const newTodos = prevTodos.filter((todo) => {
+        return todo.id !== id;
+      });
+      return newTodos;
+    });
   }
 
   return (
