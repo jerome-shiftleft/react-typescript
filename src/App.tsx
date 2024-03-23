35152f324b0dbc6e979d9900817a5bf5211ca4d1
jerome-shiftleft <jerome@theshiftleft.com>
2024-03-24 01:44:13 +0800

    Refactor App component to use useState hook for managing todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
+import { useState } from "react";
 import Todos from "./components/Todos";
 import Todo from "./models/todo";
 import NewTodo from "./components/NewTodo";
 import "./sass/App.scss";
 
-const todos = [new Todo("Learn React"), new Todo("Learn Typescript")];
-console.log("todos: ", todos);
-
 function App() {
-  const addTodoHandler = (text: string) => {
-    console.log(`final text: ${text}`);
+
+
+
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodoHandler = (todoText: string) => {
+    console.log(`final text: ${todoText}`);
+    const newTodo = new Todo(todoText)
+    setTodos((prevTodos) => {
+      //return prevTodos.concat(newTodo);
+      return [...prevTodos, newTodo];
+    });
   };
 
   return (
