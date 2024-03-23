d692d3309463f9af1d747db83316de47843ec481
jerome-shiftleft <jerome@theshiftleft.com>
2024-03-23 23:30:39 +0800

    progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Generics from "./lectures/Generics";
 import Todos from "./components/Todos";
 import Todo from "./models/todo";
+import NewTodo from "./components/NewTodo";
 import "./sass/App.scss";
 
 Generics();
@@ -11,6 +12,7 @@ console.log('todos: ', todos);
 function App() {
   return (
     <>
+      <NewTodo />
       <Todos items={todos} bgColor="#666">
         <h2>Todos</h2>
       </Todos>
