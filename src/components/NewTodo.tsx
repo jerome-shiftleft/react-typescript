e5832f3fcba4982790ef31f590a9a6a445cf6dcf
jerome-shiftleft <jerome@theshiftleft.com>
2024-03-24 00:31:01 +0800

    Add useRef hook to NewTodo component

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,14 +1,21 @@
+import { useRef } from "react";
+
 const NewTodo = () => {
+  const todoTextInputRef = useRef<HTMLInputElement>(null);
+
   const submitHandler = (event: React.FormEvent) => {
     event.preventDefault();
     console.log('submitting...');
+    //const enteredText = todoTextInputRef.current!.value;
+    const enteredText = todoTextInputRef.current?.value;
+    console.log(`enteredText: ${enteredText}`);
   }
 
   return (
     <>
       <form onSubmit={submitHandler}>
         <label htmlFor="text"></label>
-        <input type="text" id="text" />
+        <input ref={todoTextInputRef} type="text" id="text" />
         <button>Add Todo</button>
       </form>
     </>
