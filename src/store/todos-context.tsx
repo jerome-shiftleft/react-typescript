3190f99d30f13ca53fe779fd7c0dae81d1c3f992
jerome-shiftleft <jerome@theshiftleft.com>
2024-03-24 06:28:39 +0800

    Add TodosContext and TodosContextProvider components

---
+++ src/store/todos-context.tsx
@@ -0,0 +1,47 @@
+import { useState, createContext } from "react";
+import Todo from "../models/todo";
+
+type TodoContextTypes = {
+  items: Todo[];
+  addTodo: (text: string) => void;
+  removeTodo: (id: string) => void;
+};
+
+export const TodosContext = createContext<TodoContextTypes>({
+  items: [],
+  addTodo: () => {},
+  removeTodo: () => {},
+});
+
+export const TodosContextProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodoHandler = (text: string) => {
+    const newTodo = new Todo(text);
+    setTodos((prevTodos) => {
+      return [...prevTodos, newTodo];
+    });
+  };
+
+  const removeTodoHandler = (id: string) => {
+    setTodos((prevTodos) => {
+      const newTodos = prevTodos.filter((todo) => {
+        return todo.id !== id;
+      });
+
+      return newTodos;
+    });
+  };
+
+  const contextValue = {
+    items: todos,
+    addTodo: addTodoHandler,
+    removeTodo: removeTodoHandler,
+  };
+
+  return <TodosContext.Provider value={contextValue}>{children}</TodosContext.Provider>;
+};
